0f64717c31435fc53f6f19e07efb5670a9826266
Narendranath Gogineni <narenrockstar1@gmail.com>
2024-11-24 16:45:55 +0530

    activate sidepanel

--- src/content_templates/Blog/Blog.js
+++ src/content_templates/Blog/Blog.js
@@ -11,26 +11,11 @@ function Blog(selectedNode) {
             setContent(text);
         }
         fetchContent();
-        const svg = d3.select('svg');
-        const parents = svg.selectAll('g').data().find(d => d.data.id === selectedNode.parentId);
-        console.log(parents.data());
     }, [selectedNode])
     return (
         <div className="blog-content">
             {content}
             <div id="further-exploration">
-                {/* <div id="parents">
-                    <h3>Parents:</h3>
-                    {selectedNode.parents.map((parent) => {
-                        return <p>{parent.title}</p>
-                    })}
-                </div>
-                <div id="children">
-                    <h3>Children:</h3>
-                    {selectedNode.children.map((child) => {
-                        return <p>{child.title}</p>
-                    })}
-                </div> */}
             </div>
         </div>
     )
